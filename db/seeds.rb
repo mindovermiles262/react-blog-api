b60c6fadfb414b8b4f8e76371fe4e6a674518fea
Andy D <mindovermiles262@gmail>
2018-11-16 17:05:36 -0800

    add delete CORS option

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Post.delete_all if Rails.env == "development"
 
 10.times do
   Post.create!(
-    title: Faker::Hacker.ingverb,
-    author: Faker::Hacker.noun,
+    title: Faker::Book.title,
+    author: Faker::Book.author,
     body:  Faker::Hacker.say_something_smart,
-    categories: 'blog'
+    categories: Faker::Book.genre 
   )
 end
+
